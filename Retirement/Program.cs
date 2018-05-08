0b73beae3ec0b2e1e76871cbdd1b174ce87eb9c5
collectoranton <anton.lilja@collectorbank.se>
2018-05-08 13:15:30 +0200

    Add project files.

---
+++ Retirement/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Retirement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
